57650b491dde0937f0fc38131f7187e1518f0a5a
Walter Hugo Lopez Pinaya <walhugolp@gmail.com>
2018-05-07 13:08:37 +0100

    Initial commit

---
+++ _pages/news-achive.md
@@ -0,0 +1,13 @@
+---
+title: "News and Press"
+permalink: /news/
+classes: wide
+author_profile: false
+---
+
+**May 2018**  
+Andrea Mechelli invited to present at the "The Promise of Deep Learning for Psychiatry" Symposium!  
+Cho Kang Ik and Kirsten Lee joins the MLMH's group!  
+
+**April 2018**  
+New website for the MLMH Lab!  
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,21 @@
+var store = [{
+        "title": "Bao Do",
+        "excerpt":"Dr. Do is a a Clinical Assistant Professor in Radiology in the Stanford School of Medicine. His research interests include natural language processing of imaging and biomedical text in electronic health records, building real-time decision support systems using qualitative and quantitative features of imaging data, retrospective data mining of imaging...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4001/members/walter/",
+        "teaser":"http://localhost:4001/assets/images/huang_research.png"},{
+        "title": "Using deep learning to investigate the neuroimaging correlates of psychiatric and neurological disorders: Methods and applications",
+        "excerpt":"Authors: Sandra Vieira, Walter HL Pinaya, Andrea Mechelli Journal: Neuroscience &amp; Biobehavioral Reviews DOI: 10.1016/j.neubiorev.2017.01.002 Abstract: Deep learning (DL) is a family of machine learning methods that has gained considerable attention in the scientific community, breaking benchmark records in areas such as speech and visual recognition. DL differs from conventional...","categories": [],
+        "tags": ["Deep learning","Machine learning","Neuroimaging","Pattern recognition","Multilayer perceptron","Autoencoders","Convolutional neural networks","Deep belief networks","Psychiatric disorders","Neurologic disorders"],
+        "url": "http://localhost:4001/publications/2017-03-01-j.neubiorev.2017.01.002/",
+        "teaser":"http://localhost:4001/assets/images/publications/neuro_bio_rev.jpg"},{
+        "title": "Using Convolutional Neural Networks to Examine Structural Abnormalities in Schizophrenia",
+        "excerpt":"Authors: Sandra Vieira, Walter HL Pinaya, Andrea Mechelli Journal: Schizophrenia Bulletin DOI: 10.1093/schbul/sbx024.078 https://doi.org/10.1016/j.biopsych.2018.02.221 Abstract: Deep learning (DL) is a family of machine learning methods that has gained considerable attention in the scientific community, breaking benchmark records in areas such as speech and visual recognition. DL differs from conventional machine...","categories": [],
+        "tags": ["Deep learning","Convolutional neural networks"],
+        "url": "http://localhost:4001/publications/2017-03-20-sbx024.077/",
+        "teaser":"http://localhost:4001/assets/images/publications/scibull.png"},{
+        "title": "Deep Learning Technology: Concepts and Applications in Biological Psychiatry",
+        "excerpt":"Authors: Andrea Mechelli Journal: Biological Psychiatry DOI: 10.1016/j.biopsych.2018.02.221 Abstract: Technology companies such as Google, Facebook and Apple have long been using deep learning technology (DLT) for big data analysis in order to predict how people search the internet and what news they are interested in. In light of its superior...","categories": [],
+        "tags": ["Machine Learning","Deep Learning Technology","Psychosis","Neuroimaging"],
+        "url": "http://localhost:4001/publications/2018-05-01-j.biopsych.2018.02.221/",
+        "teaser":"http://localhost:4001/assets/images/publications/biopsy.png"}]
