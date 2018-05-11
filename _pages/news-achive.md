b14974bd1c0825e468d0e4b25ccab0f4137071e6
Walter Hugo Lopez Pinaya <walhugolp@gmail.com>
2018-05-11 18:27:03 +0100

    Publications updated

--- _pages/news-achive.md
+++ _pages/news-achive.md
@@ -7,7 +7,7 @@ author_profile: false
 
 **May 2018**  
 Andrea Mechelli invited to present at the "The Promise of Deep Learning for Psychiatry" Symposium!  
-Cho Kang Ik and Kirsten Lee joins the MLMH's group!  
+Kirsten Lee joins the MLMH's group!  
 
 **April 2018**  
 New website!  
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -34,13 +34,33 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4001/members/Kirsten/",
         "teaser":"http://localhost:4001/assets/images/members/Kirsten.jpg"},{
+        "title": "Prognostic prediction of therapeutic response in depression using high-field MR imaging.",
+        "excerpt":"Authors: Qiyong Gong, Qizhu Wu, Cristina Scarpazza, Su Lui, Zhiyun Jia, Andre Marquand, Xiaoqi Huang, Philip McGuire, Andrea Mechelli Journal: Neuroimage DOI: 10.1016/j.neuroimage.2010.11.079 Abstract: Despite significant advances in the treatment of major depression, there is a high degree of variability in how patients respond to treatment. Approximately 70% of patients...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4001/publications/2011-04-15-/",
+        "teaser":null},{
+        "title": "Using Support Vector Machine to identify imaging biomarkers of neurological and psychiatric disease: a critical review.",
+        "excerpt":"Authors: Graziella Orrù, William Pettersson-Yeo, Andre F. Marquand, Giuseppe Sartori, Andrea Mechelli Journal: Neuroscience &amp; Biobehavioral Reviews DOI: 10.1016/j.neubiorev.2012.01.004 Abstract: Standard univariate analysis of neuroimaging data has revealed a host of neuroanatomical and functional differences between healthy individuals and patients suffering a wide range of neurological and psychiatric disorders. Significant...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4001/publications/2012-04-01-/",
+        "teaser":"http://localhost:4001/assets/images/publications/neuro_bio_rev.jpg"},{
+        "title": "Using genetic, cognitive and multi-modal neuroimaging data to identify ultra-high-risk and first-episode psychosis at the individual level",
+        "excerpt":"Authors: W. Pettersson-Yeo, S. Benetti, Andre Marquand, Flavio Dell’ Acqua, S.C.R. Williams, P. Allen, Diana Prata, P. McGuire, A. Mechelli Journal: Psychological Medicine DOI: 10.1017/S003329171300024X Abstract: Background Group-level results suggest that relative to healthy controls (HCs), ultra-high-risk (UHR) and first-episode psychosis (FEP) subjects show alterations in neuroanatomy, neurofunction and cognition...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4001/publications/2013-03-14-/",
+        "teaser":null},{
+        "title": "An empirical comparison of different approaches for combining multimodal neuroimaging data with support vector machine",
+        "excerpt":"Authors: William Pettersson-Yeo, Stefania Benetti, Andre F. Marquand, Richard Joules, Marco Catani, Steve C. R. Williams, Paul Allen, Philip McGuire, Andrea Mechelli Journal: Frontiers in Neuroscience DOI: 10.3389/fnins.2014.00189 Abstract: In the pursuit of clinical utility, neuroimaging researchers of psychiatric and neurological illness are increasingly using analyses, such as support vector...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4001/publications/2014-07-15-/",
+        "teaser":null},{
         "title": "Using deep learning to investigate the neuroimaging correlates of psychiatric and neurological disorders: Methods and applications",
         "excerpt":"Authors: Sandra Vieira, Walter HL Pinaya, Andrea Mechelli Journal: Neuroscience &amp; Biobehavioral Reviews DOI: 10.1016/j.neubiorev.2017.01.002 Abstract: Deep learning (DL) is a family of machine learning methods that has gained considerable attention in the scientific community, breaking benchmark records in areas such as speech and visual recognition. DL differs from conventional...","categories": [],
         "tags": ["Deep learning","Machine learning","Neuroimaging","Pattern recognition","Multilayer perceptron","Autoencoders","Convolutional neural networks","Deep belief networks","Psychiatric disorders","Neurologic disorders"],
         "url": "http://localhost:4001/publications/2017-03-01-j.neubiorev.2017.01.002/",
         "teaser":"http://localhost:4001/assets/images/publications/neuro_bio_rev.jpg"},{
         "title": "Using Convolutional Neural Networks to Examine Structural Abnormalities in Schizophrenia",
-        "excerpt":"Authors: Sandra Vieira, Walter HL Pinaya, Andrea Mechelli Journal: Schizophrenia Bulletin DOI: 10.1093/schbul/sbx024.078 https://doi.org/10.1016/j.biopsych.2018.02.221 Abstract: Deep learning (DL) is a family of machine learning methods that has gained considerable attention in the scientific community, breaking benchmark records in areas such as speech and visual recognition. DL differs from conventional machine...","categories": [],
+        "excerpt":"Authors: Sandra Vieira, Walter HL Pinaya, Andrea Mechelli Journal: Schizophrenia Bulletin DOI: 10.1093/schbul/sbx024.078 Abstract: Deep learning (DL) is a family of machine learning methods that has gained considerable attention in the scientific community, breaking benchmark records in areas such as speech and visual recognition. DL differs from conventional machine learning...","categories": [],
         "tags": ["Deep learning","Convolutional neural networks"],
         "url": "http://localhost:4001/publications/2017-03-20-sbx024.077/",
         "teaser":"http://localhost:4001/assets/images/publications/scibull.png"},{
