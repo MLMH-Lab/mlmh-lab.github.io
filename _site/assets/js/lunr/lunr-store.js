71fb4800300386857fc2aded822cd1102c08af81
Rafael <rafaelagd@gmail.com>
2021-01-14 16:15:35 +0000

    Change background color to neurofind theme

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,121 +1,131 @@
 var store = [{
-        "title": "Kirsten Lee",
-        "excerpt":"","categories": [],
-        "tags": [],
-        "url": "http://localhost:4001/members/Kirsten/",
-        "teaser":"http://localhost:4001/assets/images/members/Kirsten.jpg"},{
         "title": "Andrea Mechelli",
         "excerpt":"The focus of my research is the identification of predictors of disease onset and treatment response in psychosis. This involves the application of machine learning methods to genetic, multi-modal imaging, cognitive and psychosocial data. A core aim of my programme is to exploit the translational potential of this information, using...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/members/Andrea/",
-        "teaser":"http://localhost:4001/assets/images/members/Andrea.jpg"},{
+        "url": "http://localhost:4000/members/Andrea/",
+        "teaser":"http://localhost:4000/assets/images/members/Andrea.jpg"},{
         "title": "Cristina Scarpazza",
         "excerpt":"","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/members/Cristina/",
-        "teaser":"http://localhost:4001/assets/images/members/Cristina.jpg"},{
+        "url": "http://localhost:4000/members/Cristina/",
+        "teaser":"http://localhost:4000/assets/images/members/Cristina.jpg"},{
         "title": "Walter Hugo Lopez Pinaya",
         "excerpt":"Hello there! I am a computer scientist and an engineer with a PhD in Neuroscience and Cognition from Universidade Federal do ABC (Brazil). I have a long-time interest in machine learning methods (especially Deep Learning) and neuroimaging. Besides my job in the lab, I have a position as a Assistant...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/members/Walter/",
-        "teaser":"http://localhost:4001/assets/images/members/Walter.jpg"},{
+        "url": "http://localhost:4000/members/Walter/",
+        "teaser":"http://localhost:4000/assets/images/members/Walter.jpg"},{
         "title": "Sandra Vieira",
         "excerpt":"Sandra Vieira is a researcher at the Institute of Psychiatry at King’s College London. She received her first Masters in Clinical Psychology in 2011 from the University of Coimbra, Portugal, and her second Masters in Psychiatric Research in 2014 from King’s College London. United Kingdom. She is currently completing her...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/members/Sandra/",
-        "teaser":"http://localhost:4001/assets/images/members/Sandra.jpg"},{
+        "url": "http://localhost:4000/members/Sandra/",
+        "teaser":"http://localhost:4000/assets/images/members/Sandra.jpg"},{
         "title": "Lucie Burgess",
         "excerpt":"","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/members/Lucie/",
-        "teaser":"http://localhost:4001/assets/images/members/Lucie.jpg"},{
+        "url": "http://localhost:4000/members/Lucie/",
+        "teaser":"http://localhost:4000/assets/images/members/Lucie.jpg"},{
         "title": "Ryan Hammoud",
         "excerpt":"","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/members/Ryan/",
-        "teaser":"http://localhost:4001/assets/images/members/Ryan.jpg"},{
+        "url": "http://localhost:4000/members/Ryan/",
+        "teaser":"http://localhost:4000/assets/images/members/Ryan.jpg"},{
         "title": "Rafael Garcia-Dias",
         "excerpt":"Currently, my main focus is to develop machine learning models to diagnose patients based on structural MRI. In general, my main interest is to benefit from the huge amount of data being acquired in neuroscience, using machine learning, to make a positive impact on the field. I am a PhD...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/members/Rafael/",
-        "teaser":"http://localhost:4001/assets/images/members/Rafael.jpg"},{
+        "url": "http://localhost:4000/members/Rafael/",
+        "teaser":"http://localhost:4000/assets/images/members/Rafael.jpg"},{
         "title": "Lea Baecker",
         "excerpt":"As part of my MSc Neuroscience (Neuroimaging) degree at King’s College London, I explored the use of Support Vector Machine models for the differential classification of psychosis, depression and post-traumatic stress disorder in MRI data. These psychiatric disorders tend to present with overlapping symptoms, so they are difficult to distinguish...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/members/Lea/",
-        "teaser":"http://localhost:4001/assets/images/members/Lea.jpg"},{
+        "url": "http://localhost:4000/members/Lea/",
+        "teaser":"http://localhost:4000/assets/images/members/Lea.jpg"},{
         "title": "Prognostic prediction of therapeutic response in depression using high-field MR imaging",
         "excerpt":"Authors: Qiyong Gong, Qizhu Wu, Cristina Scarpazza, Su Lui, Zhiyun Jia, Andre Marquand, Xiaoqi Huang, Philip McGuire, Andrea Mechelli Journal: Neuroimage DOI: 10.1016/j.neuroimage.2010.11.079 Abstract: Despite significant advances in the treatment of major depression, there is a high degree of variability in how patients respond to treatment. Approximately 70% of patients...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/publications/2011-04-15/",
+        "url": "http://localhost:4000/publications/2011-04-15/",
         "teaser":null},{
         "title": "Using Support Vector Machine to identify imaging biomarkers of neurological and psychiatric disease: a critical review.",
         "excerpt":"Authors: Graziella Orrù, William Pettersson-Yeo, Andre F. Marquand, Giuseppe Sartori, Andrea Mechelli Journal: Neuroscience &amp; Biobehavioral Reviews DOI: 10.1016/j.neubiorev.2012.01.004 Abstract: Standard univariate analysis of neuroimaging data has revealed a host of neuroanatomical and functional differences between healthy individuals and patients suffering a wide range of neurological and psychiatric disorders. Significant...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/publications/2012-04-01/",
-        "teaser":"http://localhost:4001/assets/images/publications/neuro_bio_rev.jpg"},{
+        "url": "http://localhost:4000/publications/2012-04-01/",
+        "teaser":"http://localhost:4000/assets/images/publications/neuro_bio_rev.jpg"},{
         "title": "Using genetic, cognitive and multi-modal neuroimaging data to identify ultra-high-risk and first-episode psychosis at the individual level",
         "excerpt":"Authors: W. Pettersson-Yeo, S. Benetti, Andre Marquand, Flavio Dell’ Acqua, S.C.R. Williams, P. Allen, Diana Prata, P. McGuire, A. Mechelli Journal: Psychological Medicine DOI: 10.1017/S003329171300024X Abstract: Background Group-level results suggest that relative to healthy controls (HCs), ultra-high-risk (UHR) and first-episode psychosis (FEP) subjects show alterations in neuroanatomy, neurofunction and cognition...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/publications/2013-03-14/",
+        "url": "http://localhost:4000/publications/2013-03-14/",
         "teaser":null},{
         "title": "Using structural neuroimaging to make quantitative predictions of symptom progression in individuals at ultra-high risk for psychosis",
         "excerpt":"Authors: Stefania Tognin, William Pettersson-Yeo, Isabel Valli, Chloe Hutton, James Woolley, Paul Allen, Philip McGuire, Andrea Mechelli Journal: Frontiers in Psychiatry DOI: 10.3389/fpsyt.2013.00187 Abstract: Neuroimaging holds the promise that it may one day aid the clinical assessment of individual psychiatric patients. However, the vast majority of studies published so far...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/publications/2014-01-29/",
+        "url": "http://localhost:4000/publications/2014-01-29/",
         "teaser":null},{
         "title": "Quantitative Prediction of Individual Psychopathology in Trauma Survivors Using Resting-State fMRI",
         "excerpt":"Authors: Qiyong Gong, Lingjiang Li, Mingying Du, William Pettersson-Yeo, Nicolas Crossley, Xun Yang, Jing Li, Xiaoqi Huang, Andrea Mechelli Journal: Neuropsychopharmacology DOI: 10.1038/npp.2013.251 Abstract: Neuroimaging techniques hold the promise that they may one day aid the clinical assessment of individual psychiatric patients. However, the vast majority of studies published so...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/publications/2014-01-01/",
+        "url": "http://localhost:4000/publications/2014-01-01/",
         "teaser":null},{
         "title": "An empirical comparison of different approaches for combining multimodal neuroimaging data with support vector machine",
         "excerpt":"Authors: William Pettersson-Yeo, Stefania Benetti, Andre F. Marquand, Richard Joules, Marco Catani, Steve C. R. Williams, Paul Allen, Philip McGuire, Andrea Mechelli Journal: Frontiers in Neuroscience DOI: 10.3389/fnins.2014.00189 Abstract: In the pursuit of clinical utility, neuroimaging researchers of psychiatric and neurological illness are increasingly using analyses, such as support vector...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/publications/2014-07-15/",
+        "url": "http://localhost:4000/publications/2014-07-15/",
         "teaser":null},{
         "title": "Predictors of treatment response in young people at ultra-high risk for psychosis who received long-chain omega-3 fatty acids.",
         "excerpt":"Authors: G P Amminger, Andrea Mechelli, S Rice, S-W Kim, C M Klier, R K McNamara, M Berk, P D McGorry, M R Schäfer Journal: Translational Psychiatry DOI: 10.1038/tp.2014.134 Abstract: Previous efforts in the prospective evaluation of individuals who experience attenuated psychotic symptoms have attempted to isolate mechanisms underlying the...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/publications/2015-01-01/",
+        "url": "http://localhost:4000/publications/2015-01-01/",
         "teaser":null},{
         "title": "Can neuroimaging be used to predict the onset of psychosis?",
         "excerpt":"Authors: Philip McGuire, Joao R Sato, Andrea Mechelli, Andrea Jackowski, Rodrigo A Bressan, Andre Zugman Journal: Lancet Psychiatry DOI: 10.1016/S2215-0366(15)00308-9 Abstract: The onset of psychotic disorders is preceded by a high-risk phase characterised by attenuated or brief psychotic symptoms and a marked decline in functioning. About a third of individuals...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/publications/2015-12-01/",
+        "url": "http://localhost:4000/publications/2015-12-01/",
         "teaser":null},{
         "title": "Using deep belief network modelling to characterize differences in brain morphometry in schizophrenia",
         "excerpt":"Authors: Walter H L Pinaya, Ary Gadelha, Orla M Doyle, Cristiano Noto, André Zugman, Quirino Cordeiro, Andrea P Jackowski, Rodrigo A Bressan, João R Sato Journal: Scientific reports DOI: 10.1038/srep38897 Abstract: Neuroimaging-based models contribute to increasing our understanding of schizophrenia pathophysiology and can reveal the underlying characteristics of this and...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/publications/2016-12-12/",
+        "url": "http://localhost:4000/publications/2016-12-12/",
         "teaser":null},{
         "title": "Using deep learning to investigate the neuroimaging correlates of psychiatric and neurological disorders: Methods and applications",
         "excerpt":"Authors: Sandra Vieira, Walter HL Pinaya, Andrea Mechelli Journal: Neuroscience &amp; Biobehavioral Reviews DOI: 10.1016/j.neubiorev.2017.01.002 Abstract: Deep learning (DL) is a family of machine learning methods that has gained considerable attention in the scientific community, breaking benchmark records in areas such as speech and visual recognition. DL differs from conventional...","categories": [],
         "tags": ["Deep learning","Machine learning","Neuroimaging","Pattern recognition","Multilayer perceptron","Autoencoders","Convolutional neural networks","Deep belief networks","Psychiatric disorders","Neurologic disorders"],
-        "url": "http://localhost:4001/publications/2017-03-01/",
-        "teaser":"http://localhost:4001/assets/images/publications/neuro_bio_rev.jpg"},{
+        "url": "http://localhost:4000/publications/2017-03-01/",
+        "teaser":"http://localhost:4000/assets/images/publications/neuro_bio_rev.jpg"},{
         "title": "Using Convolutional Neural Networks to Examine Structural Abnormalities in Schizophrenia",
         "excerpt":"Authors: Sandra Vieira, Walter HL Pinaya, Andrea Mechelli Journal: Schizophrenia Bulletin DOI: 10.1093/schbul/sbx024.078 Abstract: Deep learning (DL) is a family of machine learning methods that has gained considerable attention in the scientific community, breaking benchmark records in areas such as speech and visual recognition. DL differs from conventional machine learning...","categories": [],
         "tags": ["Deep learning","Convolutional neural networks"],
-        "url": "http://localhost:4001/publications/2017-03-20/",
-        "teaser":"http://localhost:4001/assets/images/publications/scibull.png"},{
+        "url": "http://localhost:4000/publications/2017-03-20/",
+        "teaser":"http://localhost:4000/assets/images/publications/scibull.png"},{
         "title": "Using clinical information to make individualized prognostic predictions in people at ultra high risk for psychosis",
         "excerpt":"Authors: Andrea Mechelli, Ashleigh Lin, Stephen Wood, Patrick McGorry, Paul Amminger, Stefania Tognin, Philip McGuire, Jonathan Young, Barnaby Nelson, Alison Yungh Journal: Schizophrenia Research DOI: 10.1016/j.schres.2016.11.047 Abstract: Recent studies have reported an association between psychopathology and subsequent clinical and functional outcomes in people at ultra-high risk (UHR) for psychosis. This...","categories": [],
         "tags": [],
-        "url": "http://localhost:4001/publications/2017-06-01-/",
+        "url": "http://localhost:4000/publications/2017-06-01-/",
         "teaser":null},{
         "title": "Deep Learning Technology: Concepts and Applications in Biological Psychiatry",
         "excerpt":"Authors: Andrea Mechelli Journal: Biological Psychiatry DOI: 10.1016/j.biopsych.2018.02.221 Abstract: Technology companies such as Google, Facebook and Apple have long been using deep learning technology (DLT) for big data analysis in order to predict how people search the internet and what news they are interested in. In light of its superior...","categories": [],
         "tags": ["Machine Learning","Deep Learning Technology","Psychosis","Neuroimaging"],
-        "url": "http://localhost:4001/publications/2018-05-01/",
-        "teaser":"http://localhost:4001/assets/images/publications/biopsy.jpg"},{
+        "url": "http://localhost:4000/publications/2018-05-01/",
+        "teaser":"http://localhost:4000/assets/images/publications/biopsy.jpg"},{
         "title": "Using deep autoencoders to identify abnormal brain structural patterns in neuropsychiatric disorders: A large‐scale multi‐sample study",
         "excerpt":"Authors: Walter HL Pinaya, Andrea Mechelli, Joao R Sato Journal: Human Brain Mapping DOI: 10.1002/hbm.24423 Abstract: Machine learning is becoming an increasingly popular approach for investigating spatially distributed and subtle neuroanatomical alterations in brain‐based disorders. However, some machine learning models have been criticized for requiring a large number of cases...","categories": [],
         "tags": ["Autism spectrum disorder","Computational psychiatry","Deep autoencoder","Deep learning","Schizophrenia"],
-        "url": "http://localhost:4001/publications/2018-10-11/",
-        "teaser":"http://localhost:4001/assets/images/publications/hbm.jpg"},{
+        "url": "http://localhost:4000/publications/2018-10-11/",
+        "teaser":"http://localhost:4000/assets/images/publications/hbm.jpg"},{
         "title": "Using Machine Learning and Structural Neuroimaging to Detect First Episode Psychosis: Reconsidering the Evidence",
         "excerpt":"Authors: Sandra Vieira, Qi-yong Gong, Walter HL Pinaya, Cristina Scarpazza, Stefania Tognin, Benedicto Crespo-Facorro, Diana Tordesillas-Gutierrez, ictor Ortiz-García, Esther Setien-Suero, Floortje E. Scheepers, Neeltje E. M. van Haren, Tiago R. Marques, Robin M. Murray, Anthony David, Paola Dazzan, Philip McGuire, Andrea Mechelli Journal: Schizophrenia Bulletin DOI: 10.1093/schbul/sby189 Abstract: Despite the...","categories": [],
         "tags": ["Multivariate pattern recognition","Classification","Psychosis","Neuroimaging","Multi-site"],
-        "url": "http://localhost:4001/publications/2019-02-27/",
-        "teaser":"http://localhost:4001/assets/images/publications/scibull.png"}]
+        "url": "http://localhost:4000/publications/2019-02-27/",
+        "teaser":"http://localhost:4000/assets/images/publications/scibull.png"},{
+        "title": "Detecting schizophrenia at the level of the individual: relative diagnostic value of whole-brain images, connectome-wide functional connectivity and graph-based metrics",
+        "excerpt":"Authors: Du Lei, Walter HL Pinaya, Therese van Amelsvoort, Machteld Marcelis, Gary Donohoe, David O. Mothersill, Aiden Corvin, Michael Gill, Sandra Vieira, Xiaoqi Huang, Su Lui, Cristina Scarpazza, Jonathan Young, Celso Arango, Ed Bullmore, Qiyong Gong, Philip McGuire, Andrea Mechelli Journal: Psychological Medicine DOI: 10.1017/S0033291719001934 Abstract:Background:Previous studies using resting-state functional...","categories": [],
+        "tags": ["Functional connectivity","Graph theoretical analysis","Machine learning","Neuroimaging","Schizophrenia"],
+        "url": "http://localhost:4000/publications/2019-08-08/",
+        "teaser":"http://localhost:4000/assets/images/publications/hbm.png"},{
+        "title": "Integrating machining learning and multimodal neuroimaging to detect schizophrenia at the level of the individual",
+        "excerpt":"Authors: Du Lei, Walter HL Pinaya, Jonathan Young, Therese van Amelsvoort, Machteld Marcelis, Gary Donohoe, David O. Mothersill, Aiden Corvin, Sandra Vieira, Xiaoqi Huang, Su Lui,Cristina Scarpazza, Celso Arango, Ed Bullmore, Qiyong Gong, Philip McGuire, Andrea Mechelli Journal: Human Brain Mapping DOI: 10.1002/hbm.24863 Abstract: Schizophrenia is a severe psychiatric disorder...","categories": [],
+        "tags": ["Functional connectivity","Graph theoretical analysis","Machine learning","Neuroimaging","Schizophrenia"],
+        "url": "http://localhost:4000/publications/2019-11-18/",
+        "teaser":"http://localhost:4000/assets/images/publications/hbm.png"},{
+        "title": "From models to tools: clinical translation of machine learning studies in psychosis",
+        "excerpt":"*Authors: Andrea Mechelli, Sandra Vieira Journal: npj Schizophrenia DOI: 10.1038/s41537-020-0094-8 ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/publications/2020-02-14/",
+        "teaser":null}]
